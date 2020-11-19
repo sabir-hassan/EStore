02b6e3a3767fe60d3430160b1de3df91834661a9
sabirhassan <l164258@lhr.nu.edu.pk>
2020-11-19 19:08:16 +0500

    feat: add auth config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  acts_as_token_authentication_handler_for User, fallback: :none
+  
 
 #  before_action :current_cart
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  
+  acts_as_token_authenticatable
+
   has_many :orders, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
