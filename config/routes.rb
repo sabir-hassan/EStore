1bfcbd07e231ddc772fc7c47fae3ae5fd8e15b33
sabirhassan <l164258@lhr.nu.edu.pk>
2020-11-19 19:06:28 +0500

    feat: add cors configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'categories#index'
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :cart, only: [:index, :create, :destroy]
+      resources :order, only: [:create]
+      resources :categories, only: [:index] do
+        resources :products, only: [:index]
+      end
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
+    end
+  end
   
+
+
   post '/search', to: 'products#index'
 
- devise_scope :user do
-  get '/users/sign_out', to:'devise/sessions#destroy'
- end
+  devise_scope :user do
+    get '/users/sign_out', to:'devise/sessions#destroy'
+  end
 
 #  ActiveAdmin.routes(self)
 
